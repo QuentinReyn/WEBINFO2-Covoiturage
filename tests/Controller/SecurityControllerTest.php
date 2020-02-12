c58b58dd064cf610e5daff7bea80745c166a13ce
QuentinR <quentin.reynaud99@gmail.com>
2020-02-12 15:00:45 +0100

    homepage and registration form & tests

--- tests/Controller/SecurityControllerTest.php
+++ tests/Controller/SecurityControllerTest.php
@@ -11,7 +11,14 @@ class SecurityControllerTest extends WebTestCase
 {
     public function testLogin(){
         $client = static::createClient();
-        $client->request('/login');
+        $client->request("GET",'/login');
+        $this->assertEquals(200,$client->getResponse()->getStatusCode());
+    }
+
+    public function testLogout(){
+        $client = static ::createClient();
+        $client->followRedirects();
+        $client->request("GET","/Logout");
         $this->assertEquals(200,$client->getResponse()->getStatusCode());
     }
 }
